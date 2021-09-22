f5104ad05defb1765ef2f8aa3b3a3f91ee5464e2
Patrisha de Boon <ttdeboon@gmail.com>
2021-09-21 23:53:33 -0600

    Add functionality to server.py

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 #  coding: utf-8 
 import socketserver
+import mimetypes
+from datetime import date
+from os import path
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,13 +29,184 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+dateFormat = "%a, %d %b %Y %H:%M:%S GMT"
+
+class Request():
+    def __init__(self):
+        self.protocol: str = "HTTP/1.1"
+        self.method: str = "GET"
+        self.resource: str = None
+        self.headers: dict = {}
+
+class Response():
+    def __init__(self):
+        self.protocol: str = "HTTP/1.1"
+        self.headers: dict = {"Connection": "close"}
+        self.data = None
+        self.status: str= "200 OK"
+
+    def compileResponse(self):
+        strResponse = self.protocol + " " + self.status + "\r\n"
+        if (not self.headers):
+            self.headers = {}
+        self.headers["Date"] = date.today().strftime(dateFormat)
+
+        if self.data:
+            self.headers["Content-Length"] = str(len(self.data))
+
+        for name, value in self.headers.items():
+            strResponse += name + ": " + value + "\r\n"
+        
+        strResponse += "\r\n" 
+
+        if self.data:
+            strResponse += self.data
+
+        return strResponse
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+    root = path.dirname(path.abspath(__file__)) +  "/www"
+
+    """
+        parse the data into a Response object
+    """
+    def parseData(self):
+        lines = self.data.decode("utf-8").split("\r\n")
+        request = Request()
+        isFirst = True
+        for line in lines:
+            split = None
+            if isFirst:
+                split = line.strip().split(" ")
+                if (len(split) == 3):
+                    request.resource = split[1]
+                    request.protocol = split[2]
+                    method = split[0].lower()
+                    if (method == "get"):
+                        request.method = "GET"
+                    else:
+                        request.method = split[0]
+                else:
+                    # TODO throw bad request error
+                    print("request line has < or > 3 space seperated arguments")
+                isFirst = False
+            else:
+                split = line.strip().split(":")
+
+                if split:
+                    # remove empty strings and extra whitespace
+                    split[:] = [x.strip() for x in split if x.strip() != ""]
+                
+                if split and len(split) > 0:
+                    val = None
+                    if (len(split) > 1 ):
+                        val = split[1]
+                    request.headers[split[0]] = val
+        return request
+
+    """
+        Gat a tuple where the first field is the absolute path to a file, or the path to the new location 
+        to be returned with a 301 code, or None if the file doesn't exit. The second field is the status code
+        to return for the file.
+    """
+    def getFilePath(self, originalPath: str):
+        filePath = self.root + "/" + originalPath.lstrip("\\").lstrip("/")
+        if (path.commonpath([self.root, path.abspath(filePath)]) == self.root):
+            # Account for symbolic links as long as the link is located in the root.
+            filePath = path.abspath(path.realpath(filePath))
+            if (path.isdir(filePath)):
+                if (originalPath.endswith("/")):
+                    # default to index.html if the path is a directory with a valid ending
+                    filePath += "/index.html"
+                else:
+                    # redirect if the path is a directory with an invalid ending
+                    return (originalPath + "/", 301)
+
+            if (path.exists(filePath)):
+                return (filePath, 200)
+        return (None, 404)
+
+    """
+        Get the encoding and content type for the file, 
+        set the Content-Type header, and return the charset to use
+        when opening the file
+    """
+    def getContentType(self, res: Response, filePath: str):
+        contentType = mimetypes.guess_type(filePath, strict=True)
+        encoding = "utf-8"
+        if contentType and contentType[0]:
+            if contentType[1]:
+                encoding = contentType[1]
+            res.headers["Content-Type"] = contentType[0] + "; charset=" + encoding
+        else:
+            res.headers["Content-Type"] = "text/plain; charset=" + encoding
+        return encoding
+
+    """
+        Create a response including the requested resource if possible,
+        or the proper error codes and messages if not possible
+    """
+    def handleGet(self, req: Request):
+        res = Response()
+        if (req.resource):
+            filePath, statusCode = self.getFilePath(req.resource)
+            if (filePath == None):
+                res.status = "404 Not Found"
+                res.data = "Could not find the requested resource"
+            elif(statusCode == 200):
+                file = open(filePath, "r", encoding=self.getContentType(res, filePath))
+                res.data = file.read()
+                res.headers["Last-Modified"] = date.fromtimestamp(path.getmtime(filePath)).strftime(dateFormat)
+                res.status = "200 OK"
+            elif(statusCode == 301):
+                res.status = "301 Moved Permanently"
+                res.data = "The requested file has permanently moved"
+                res.headers["Location"] = filePath
+        else:
+            res.status = "400 Bad Request"
+            res.data = "No Resource URI was included in the request"
+        return res
+
+    """
+        Create a response with the 405 status code
+    """
+    def handleMethodNotAllowed(self):
+        res = Response()
+        res.status = "405 Method Not Allowed"
+        res.headers["Allow"] = "GET"
+        return res
+
+    """
+        Create a response with the 403 status code
+    """
+    def handleBadProtocol(self):
+        res = Response()
+        res.status = "403 Forbidden"
+        res.data = "Unsupported Protocol"
+        return res
+
+    """
+        Handle a given user request
+    """
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        print ("Got a request of: %s" % self.data)
+        res: Response = None
+        if (self.data):
+            req = self.parseData()
+            if (req.protocol != "HTTP/1.1"):
+                res = self.handleBadProtocol()
+
+            if (req.method == "GET"):
+                res = self.handleGet(req)
+            else:
+                res = self.handleMethodNotAllowed()
+
+        if (res):
+            strResponse = res.compileResponse()
+            self.request.sendall(bytearray(strResponse, "utf-8"))
+            print(strResponse)
+        print()
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
